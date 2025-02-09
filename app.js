1fb444e0f86a5241e5db4bac6c2a19cf26f93da9
LogicMaster <traderrafa02@gmail.com>
2025-02-09 10:55:53 -0300

    initial request

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const express = require("express");
 
 const app = express();
-const itens = [];
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("seja bem vindo a API");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`SERVIDOR RODANDO NA PORTA ${PORT}`);
+});
