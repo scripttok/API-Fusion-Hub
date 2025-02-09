8184371ee35db5fe56481194aff1b2e533f29d13
LogicMaster <traderrafa02@gmail.com>
2025-02-09 15:39:05 -0300

    add method post

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const app = express();
 app.use(express.json());
 const lista = [];
 
+//teste requisição
 app.get("/", (req, res) => {
   res.send("seja bem vindo a API");
 });
-
+//lista itens
 app.get("/itens", (req, res) => {
   res.json(lista);
+});
+//adiciona itens
+app.post("/itens", (req, res) => {
+  const id = lista.length + 1;
+
+  const dadosRecebidos = req.body;
+  const meusDados = { id, ...dadosRecebidos };
+
+  lista.push(meusDados);
+
+  res.status(201).json(meusDados);
 });
 
 const PORT = 3000;
